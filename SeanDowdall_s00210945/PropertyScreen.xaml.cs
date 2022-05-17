7411a20537713872bf0f71d05aefc8f7fe2fdb51
Sean Dowdall <S00210945@mail.itsligo.ie>
2022-05-17 11:25:33 +0100

    Added Property Screen

---
+++ SeanDowdall_s00210945/PropertyScreen.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SeanDowdall_s00210945
+{
+    /// <summary>
+    /// Interaction logic for PropertyScreen.xaml
+    /// </summary>
+    public partial class PropertyScreen : Window
+    {
+        public PropertyScreen()
+        {
+            InitializeComponent();
+        }
+    }
+}
